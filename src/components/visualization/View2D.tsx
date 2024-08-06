bb83adeeb84656a04a361f01b2010ad3bd6b35e8
ChrisVivasAI <christian.vivas001@mymdc.net>
2024-08-06 17:38:35 -0400

    main

--- src/components/visualization/View2D.tsx
+++ src/components/visualization/View2D.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const View2D: React.FC = () => {
+interface View2DProps {
+  projectId: string;
+}
+
+const View2D: React.FC<View2DProps> = ({ projectId }) => {
   return (
     <div className="border p-4 rounded-lg">
       <h2 className="text-xl font-semibold mb-4">2D View</h2>
       {/* Implement Mermaid.js chart here */}
-      <p>2D visualization will be implemented here using Mermaid.js</p>
+      <p>2D visualization for project {projectId} will be implemented here using Mermaid.js</p>
     </div>
   )
 }
--- src/components/visualization/View3D.tsx
+++ src/components/visualization/View3D.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const View3D: React.FC = () => {
+interface View3DProps {
+  projectId: string;
+}
+
+const View3D: React.FC<View3DProps> = ({ projectId }) => {
   return (
     <div className="border p-4 rounded-lg">
       <h2 className="text-xl font-semibold mb-4">3D View</h2>
       {/* Implement Three.js visualization here */}
-      <p>3D visualization will be implemented here using Three.js</p>
+      <p>3D visualization for project {projectId} will be implemented here using Three.js</p>
     </div>
   )
 }
